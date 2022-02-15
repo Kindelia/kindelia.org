355702d62e8c8112027220a2d59477abced69abd
Victor Maia <victor.taelin@gmail.com>
2022-02-15 17:27:17 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,39 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// TODO: do this right
+if (window.location.href.indexOf("aprendiz") !== -1) {
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfarRCmCVD0BGKbLiIi-WQozDmmlaLWy9TkfNWOdPNGUuVlag/viewform";
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h2>
+      Kindelia
+    </h2>
+    <div>
+      Site em construção.
+    </div>
+    <h2>
+      Links
+    </h2>
+    <ul>
+      <li>
+        <a href="https://github.com/kindelia">
+          Nosso GitHub.
+        </a>
+      </li>
+      <li>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfarRCmCVD0BGKbLiIi-WQozDmmlaLWy9TkfNWOdPNGUuVlag/viewform">
+          Venha ser um Aprendiz!
+        </a>
+      </li>
+    </ul>
+  </div>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
