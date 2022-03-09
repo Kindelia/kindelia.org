cb1e1aa5a491c5269a321dd25f4fffeee22fef41
rheidner <rheidner.achiles@gmail.com>
2022-03-09 11:06:41 -0300

    add prod changes

--- src/pages/enqine/fetch.js
+++ src/pages/enqine/fetch.js
@@ -1,13 +1,17 @@
-const BASE_URL = 'http://localhost:8080';
-
-export default function fetchAbsolute(url, {...options}, ...otherParams) {
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080"
+    : ""; // TODO add production url to backend
+export default function fetchAbsolute(url, { ...options }, ...otherParams) {
   if (options.data) {
-    options.body = JSON.stringify({...options.data});
+    options.body = JSON.stringify({ ...options.data });
   }
   options.headers = {
     ...options.headers,
-    'Accept': 'application/json',
-    'Content-Type': 'application/json;charset=UTF-8'
-  }
-  return url.startsWith('/') ? fetch(BASE_URL + url, options, ...otherParams) : fetch(url, options, ...otherParams)
+    Accept: "application/json",
+    "Content-Type": "application/json;charset=UTF-8",
+  };
+  return url.startsWith("/")
+    ? fetch(BASE_URL + url, options, ...otherParams)
+    : fetch(url, options, ...otherParams);
 }
