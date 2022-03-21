8b7ba32b4b2706df6b0020be2d368fdbf4e3689d
rheidner <rheidner.achiles@gmail.com>
2022-03-21 14:07:53 -0300

    add redirects to server

--- server.js
+++ server.js
@@ -3,28 +3,36 @@ const next = require("next");
 const favicon = require("express-favicon");
 const path = require("path");
 
-const port = parseInt(process.env.PORT, 10) || 8000;
+const port = parseInt(process.env.PORT, 10) || 80;
 // const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
+const redirects = ["roadgraph", "test", "test/result"];
+
 const server = express();
+
+redirects.forEach((route) => {
+  server.all(`/${route}/`, (req, res) => {
+    res.redirect(`/apps/${route}`);
+  });
+});
 
 server.use(
   "/apps",
   favicon(path.join(__dirname, "apps", "build", "favicon.ico"))
 );
-// the __dirname is the current directory from where the script is running
-server.use("/apps", express.static(path.join(__dirname, "apps")));
-server.use("/apps", express.static(path.join(__dirname, "apps", "build")));
 
-server.get("/apps/*", function (req, res) {
+// the __dirname is the current directory from where the script is running
+// server.use("/apps", express.static(path.join(__dirname, "apps")));
+server.use("/apps/", express.static(path.join(__dirname, "apps", "build")));
+
+server.get(/\/apps\/.+/, function (req, res) {
   res.sendFile(path.join(__dirname, "apps", "build", "index.html"));
 });
 
 app.prepare().then(() => {
   server.all("*", (req, res) => {
-
     return handle(req, res);
   });
 
